0508c00841f17b4147d44da50dfef6262579b7a2
matkclar <mattclark1616@gmail.com>
2024-05-30 10:49:28 -0400

    Fixed Email & Instagram Link Errors in Foot Component

--- components/footer.js
+++ components/footer.js
@@ -8,9 +8,14 @@ class Footer extends HTMLElement {
         this.innerHTML = `
         <footer>
             <ul class="footer-links">
-                <li><a href="mattclark1616@gmail.com"><i class="fa-solid fa-envelope"></i></a> </li> <!-- Email -->
-                <li><a href="https://www.linkedin.com/in/matthew-clark-705306223/"><i class="fa-brands fa-linkedin"></i></a> </li> <!-- LinkedIn -->
-                <li><a href="https://www.instagram.com/mattclark1616/"><i class="fa-brands fa-square-instagram"></i></a> </li>
+              <!-- Email -->    
+              <li><a href="mailto: mattclark1616@gmail.com"><i class="fa-solid fa-envelope"></i></a> </li>
+              
+              <!-- LinkedIn -->
+              <li><a href="https://www.linkedin.com/in/matthew-clark-705306223/"><i class="fa-brands fa-linkedin"></i></a> </li>
+              
+              <!-- Instagram -->
+              <li><a href="https://www.instagram.com/codeclark1616/"><i class="fa-brands fa-square-instagram"></i></a> </li>
             </ul>
     
             <p class="copyright"><i class="fa-regular fa-copyright"></i> Matthew Clark 2024</p>
